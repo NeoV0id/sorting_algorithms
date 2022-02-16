edda4ff624d6e2a723c723216e17d261d50091e4
Neo <neochronos.chaos9@gmail.com>
2022-02-16 16:25:41 +0100

    Fixed 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,21 +11,19 @@ void bubble_sort(int *array, size_t size)
 	unsigned int i, j;
 	int tmp;
 
-	if (size <= 2)
+	if (size < 2)
 		return;
 	for (j = 0; j < size; j++)
 	{
 		for (i = 0; i < size; i++)
 		{
-			if (array[i] > array[i + 1])
-			{
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp;
-				print_array(array, size);
-			}
-			else
-				continue;
+				if (array[i] > array[i + 1])
+				{
+					tmp = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = tmp;
+					print_array(array, size);
+				}
 		}
 	}
 }
